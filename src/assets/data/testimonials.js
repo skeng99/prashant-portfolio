f0139f78253aeb00e7e0dede5b840eaa3897a531
skeng99 <prashantnepal143@gmail.com>
2022-04-01 16:15:30 +0545

    reactportfolio

---
+++ src/assets/data/testimonials.js
@@ -0,0 +1,27 @@
+const testimonials = [
+  {
+    id: 1,
+    name: 'Dhiran Karki',
+    title: 'CEO',
+    org: 'APAR NEPAL',
+    desc:
+      'Prashant is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
+  },
+  {
+    id: 2,
+    name: 'Shanjil shrestha',
+    title: 'UNITY DEVELOPER',
+    org: 'APAR NEPAL',
+    desc:
+      'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
+  },
+  {
+    id: 3,
+    name: 'Devesh Shrestha',
+    title: 'Wordpress Developer',
+    org: 'APAR NEPAL',
+    desc:
+      'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
+  },
+];
+export default testimonials;
